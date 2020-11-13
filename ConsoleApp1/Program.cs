2a390631190393fc63d5bc74296bc4ece8580b4f
Vladdden <Vlad2999009@yandex.ru>
2020-11-13 17:13:00 +0300

    System_Timers_Timer и System_Threading_Timer работают в одном потоке

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,9 +2,11 @@
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
+using Timer = System.Threading.Timer;
 
 namespace ConsoleApp1
 {
@@ -13,20 +15,27 @@ namespace ConsoleApp1
         static int count = 3;
         static System.Timers.Timer T1;
         static System.Threading.Timer T2;
-        
+        static AutoResetEvent waitHandler = new AutoResetEvent(true);
         static ManualResetEvent resetEvent = new ManualResetEvent(false);
+        static ManualResetEvent resetEvent2 = new ManualResetEvent(false);
+
         static void Main(string[] args)
         {
             int timeInSec = 0;
             Console.WriteLine("Введите количество секунд!");
             timeInSec = Int32.Parse(Console.ReadLine()) * 1000;
+            Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
+            System_Timers_Timer(timeInSec);
+            Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
+            System_Threading_Timer(timeInSec);
+            /*
             Thread t1 = new Thread(System_Timers_Timer);
             t1.Name = "System_Timers_Timer";
             t1.Start(timeInSec);
             Console.WriteLine("\nPress the Enter key to exit the application...\n");
             Console.WriteLine("The application started at {0:HH:mm:ss.fff}", DateTime.Now);
             
-            /*
+            
             Thread t2 = new Thread(System_Threading_Timer);
             t2.Name = "System_Threading_Timer";
             t2.Start(timeInSec);
@@ -45,6 +54,7 @@ namespace ConsoleApp1
 
         static async void Logging(TimerInfo info)
         {
+            waitHandler.WaitOne();
             //string writePath = "" + Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/log.txt";
             //@"C:\SomeDir\log.txt";
             string writePath = "/home/vladdden/RiderProjects/ConsoleApp1/ConsoleApp1/log.txt";
@@ -55,35 +65,39 @@ namespace ConsoleApp1
             string t5 = "--------------------------------------------------------------------------------------------";
             try
             {
-                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
                 {
                     await sw.WriteLineAsync(t1);
                 }
 
-                using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                using (StreamWriter sw2 = new StreamWriter(writePath, true, System.Text.Encoding.Default))
                 {
-                    await sw.WriteLineAsync(t2);
+                    await sw2.WriteLineAsync(t2);
                 }
-                using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+
+                using (StreamWriter sw3 = new StreamWriter(writePath, true, System.Text.Encoding.Default))
                 {
-                    await sw.WriteLineAsync(t3);
+                    await sw3.WriteLineAsync(t3);
                 }
+
                 if (info.timerException != null)
                 {
-                    using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                    using (StreamWriter sw4 = new StreamWriter(writePath, true, System.Text.Encoding.Default))
                     {
-                        await sw.WriteLineAsync(t4);
+                        await sw4.WriteLineAsync(t4);
                     }
                 }
-                using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+
+                using (StreamWriter sw5 = new StreamWriter(writePath, true, System.Text.Encoding.Default))
                 {
-                    await sw.WriteLineAsync(t5);
+                    await sw5.WriteLineAsync(t5);
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
+            waitHandler.Set();
         }
         
         static void System_Timers_Timer(object secVal)
@@ -113,6 +127,7 @@ namespace ConsoleApp1
                 T1.Enabled = true;
                 resetEvent.WaitOne(); // This blocks the thread until resetEvent is set
                 resetEvent.Close();
+                resetEvent.Dispose();
                 ////////////////////////////////////////////////////////////////////////////////////////////////////////
                 stopwatch.Stop(); // Останавливаем внутренний таймер объекта Stopwatch
                 stop = DateTime.Now;
@@ -126,7 +141,7 @@ namespace ConsoleApp1
             }
             finally
             {
-                Console.WriteLine("EndMain");
+                Console.WriteLine("EndT1");
                 TimerInfo timer = new TimerInfo(Thread.CurrentThread.Name, start, startTicks, stop, stopTicks, difference, timerException);
                 Logging(timer);
             }
@@ -134,7 +149,45 @@ namespace ConsoleApp1
         
         static void System_Threading_Timer(object secVal)
         {
-            
+            Console.WriteLine("start T2");
+            DateTime start = new DateTime();
+            DateTime stop = new DateTime();
+            long startTicks = 0;
+            long stopTicks = 0;
+            long difference = 0;
+            Exception timerException = null;
+            //int count_val = count;
+            TimerEndInfo timerEnd = new TimerEndInfo();
+            try
+            {
+                Stopwatch stopwatch = new Stopwatch();  // Запускаем внутренний таймер объекта Stopwatch
+                stopwatch.Start();
+                start = DateTime.Now;
+                startTicks = DateTime.Now.Ticks;
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////
+                //Thread.Sleep(100);
+                // Create a timer with a two second interval.
+                //TimerCallback tm = new TimerCallback(SomeFunc);
+                Timer timer = new Timer(SomeFunc, null, (int)secVal, Timeout.Infinite);
+                resetEvent2.WaitOne(); // This blocks the thread until resetEvent is set
+                resetEvent2.Close();
+                ////////////////////////////////////////////////////////////////////////////////////////////////////////
+                stopwatch.Stop(); // Останавливаем внутренний таймер объекта Stopwatch
+                stop = DateTime.Now;
+                stopTicks = DateTime.Now.Ticks;
+                difference = stopwatch.ElapsedTicks;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + ":" + e);
+                timerException = e;
+            }
+            finally
+            {
+                Console.WriteLine("EndT2");
+                TimerInfo timer = new TimerInfo(Thread.CurrentThread.Name, start, startTicks, stop, stopTicks, difference, timerException);
+                Logging(timer);
+            }
         }
         
         static void System_Windows_Forms_Timer(object secVal)
@@ -147,15 +200,18 @@ namespace ConsoleApp1
             
         }
         
-        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        static void OnTimedEvent(Object source, ElapsedEventArgs e)
         {
-            Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
-                e.SignalTime);
+            Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}", e.SignalTime);
             resetEvent.Set();
-            //TimerEndInfo timerEndInfo = new TimerEndInfo(DateTime.Now, DateTime.Now.Ticks);
-            //return timerEndInfo;
         }
 
+        static void SomeFunc(object o)
+        {
+            Console.WriteLine("Timer working!");
+            resetEvent2.Set();
+        }
+        
     }
     
     class TimerInfo
